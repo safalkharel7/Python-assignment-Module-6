78098ce8bbddc709cf9446152d0553ef5abd9c82
Dell <safalkharel7@gmail.com>
2026-03-14 21:36:06 +0545

    final

--- 1. dice rolls.py
+++ 1. dice rolls.py
@@ -2,7 +2,13 @@ import random #for random operations
 def dice(): #name given to the function that rolls the dice.
     dice_rolled = random.randint(1, 6) #random roll result
     return dice_rolled #returns the rolled number
-rolled_value = 0
-while rolled_value != 6:
-    rolled_value = dice() #calling dice function to execute
-    print("You rolled a ", rolled_value)
+rolled_value = dice() #first rolled number
+if rolled_value == 6:
+    print("\nYou rolled a 6 on first roll") #if 6 is achieved on first roll
+else:
+    print("\nYou rolled a " + str(rolled_value)+ " on first roll") #to show what was rolled.
+    roll = 1 #counting roll to know exactly when 6 is achieved
+    while rolled_value != 6:
+        rolled_value = dice() #calling dice function to execute
+        roll += 1 #adding to the number of rolls.
+        print("You rolled a " + str(rolled_value) + ", on roll number " + str(roll))
--- 2. n sides.py
+++ 2. n sides.py
@@ -2,11 +2,15 @@ import random #for random operations
 def dice(sides): #name given to the function that rolls the dice.
     dice_rolled = random.randint(1, sides) #random roll result. sides is defined below.
     return dice_rolled #returns the rolled number
-sides = int(input("Enter the number of sides on the dice: "))
-rolled_value = 0 #to enter the loop
-roll = 0 #to count how many dice rolls is takes.
-while rolled_value != sides:
-    rolled_value = dice(sides) #calling dice function to execute
-    roll += 1 #counting the number of dice rolls.
-    print(f"You rolled a {rolled_value}.")
+num_of_sides = int(input("Enter the number of sides on the dice: "))
+rolled_value = dice(num_of_sides) #for the first roll value. 1st dice function executed.
+if rolled_value == num_of_sides:
+    print("\nYou rolled a " + str(rolled_value) + " on first attempt.")
+else:
+    print("\nYou rolled a " + str(rolled_value) + " on first attempt.")
+    roll = 1
+    while rolled_value != num_of_sides:
+        rolled_value = dice(num_of_sides) #calling dice function to execute
+        roll += 1 #counting the number of dice rolls.
+        print(f"You rolled a {rolled_value} on roll number {roll}.")
 print(f"\nIt took {roll} dice rolls to finally get a {rolled_value}.")
